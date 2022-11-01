099ec1d4875fd5cb508e244fbf9c373d0950431e
Devika18 <devikankarpe@gmail.com>
2022-11-01 09:11:16 +0530

    added workshop program

---
+++ src/FindEvenOdd.java
@@ -0,0 +1,11 @@
+public class FindEvenOdd {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Even and Odd Program");
+        int num = 14;
+        if(num%2==0) {
+            System.out.println(num +" : is a Even Number");
+        }
+        else
+            System.out.println(num+" : is a Odd Number");
+    }
+}
